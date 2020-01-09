173a9a52d473a0d88b427e83638c88b1290464f9
Leah H <harrichl@gmail.com>
2020-01-09 15:35:48 -0500

    set up pages views using ejs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const router = express.Router();
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const ejs = require('ejs');
+
+require('dotenv').config();
 
 // include routes -- use with templating
 // const index = require('./routes/index');
@@ -12,16 +16,44 @@ const ejs = require('ejs');
 const app = express();
 
 // view engine setup
-
 app.set('view engine', 'ejs');
 
 // import settings from .env file or ENV variables
-require('dotenv').config();
+
+// look for static files in the 'public' folder
+app.use(express.static(__dirname + 'public'));
+// set up other express middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(logger('dev'));
+
+// use the route files
+//app.use('/', index);
+//app.use('/users', users);
+
+app.get('/', function(req, res) {
+    res.send("Hello World!");
+});
+
+router.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/login');
+});
+
 
 // require passport and the Strategies used
 const passport = require('passport');
 const Strategy = require('passport-strategy');
 const LinkedInStrategy = require('@sokratis/passport-linkedin-oauth2').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 passport.use(new LinkedInStrategy({
@@ -49,10 +81,29 @@ app.get('/auth/linkedin',
 
 // the login callback:
 
-    app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
         successRedirect: '/',
         failureRedirect: '/login'
     }));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('pages/error');
+});
+
 
 
 // Express and Passport Session
@@ -62,8 +113,8 @@ app.use(session({
     resave: true,
     saveUninitialized: true,
 }));
-app.use(passport.initialize());
-app.use(passport.session());
+
+
 
 // serialize = parse and store data in session
 passport.serializeUser(function(user, done) {
@@ -78,49 +129,16 @@ passport.deserializeUser(function(serializedUser, done) {
   // this function takes the serialized data and should expand it into a full user object
   // for example, maybe you are going to get the user from the database by id?
   // null is for errors
-    const user = serializedUser
+    const user = serializedUser;
     done(null, user);
 });
 
-// set up other express middleware
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-// look for static files in the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// use the route files
-//app.use('/', index);
-//app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-app.get("/", function(req, res, next) {
-    console.log('you are here!');
-});
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("listening on port 3000");
-})
+    console.log(`listening on port: ${port}`);
+});
 
 module.exports = app;
