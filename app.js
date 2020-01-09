5c8e89f3845f9db6edb9cb74f75b8ff9229bf710
Leah H <harrichl@gmail.com>
2020-01-08 22:58:59 -0500

    add linkedin auth

--- app.js
+++ app.js
@@ -20,24 +20,39 @@ require('dotenv').config();
 
 // require passport and the Strategies used
 const passport = require('passport');
+const Strategy = require('passport-strategy');
 const LinkedInStrategy = require('@sokratis/passport-linkedin-oauth2').Strategy;
 
 
 passport.use(new LinkedInStrategy({
-    clientID: process.env.LINKEDIN_API_KEY,
-    clientSecret: process.env.LINKEDIN_SECRET_KEY,
+    clientID: process.env.LINKEDIN_KEY,
+    clientSecret: process.env.LINKEDIN_SECRET,
     callbackURL: `${process.env.APP_URL}/auth/linkedin/callback`,
     scope: ['r_emailaddress', 'r_liteprofile'],
 }, function(accessToken, refreshToken, profile, done) {
-  // asynchronous verification, for effect...
+    // asynchronous verification, for effect...
     process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
+        // To keep the example simple, the user's LinkedIn profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the LinkedIn account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
     });
 }));
+
+app.get('/auth/linkedin',
+    passport.authenticate('linkedin', { state: 'SOME STATE'  }),
+    function(req, res){
+        // The request will be redirected to LinkedIn for authentication, so this
+        // function will not be called.
+    });
+
+// the login callback:
+
+    app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
 
 
 // Express and Passport Session
@@ -100,7 +115,9 @@ app.use(function(err, req, res, next) {
 
 app.get("/", function(req, res, next) {
     console.log('you are here!');
-})
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("listening on port 3000");
