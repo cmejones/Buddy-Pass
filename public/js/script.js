6b1f2d84cb421c8746c82a3a1bb2204e9ac8d44b
cmejones <1016cme@gmail.com>
2020-01-16 21:37:05 -0500

    updated models to use joins

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,10 @@ function createFunctionalArea(data) {
     return axios.post('/api/functional-area', data);
 }
 
+function createSkills(data) {
+    return axios.post('/api/skills', data);
+}
+
 function profileUpdate(data) {
     return axios
         .patch('/api/edit-profile', data);
@@ -12,3 +16,36 @@ function profileUpdate(data) {
     //         console.log(err);
     // });
 }
+
+
+// //return functional areas
+// const findFuncAreas = async function (funcAreaId) {
+//     const area = await db.functionalareas.find({
+//         where: {
+//         id: funcAreaId
+//         }
+//     })
+//     if (!area) {
+//         return 'none found'
+//     }
+//     return area.dataValues
+// }
+//return user skills
+// const findUserSkills = async function (userSkillsId) {
+//     const skills = await db.users.find({
+//         where: {
+//         id: userSkillsId
+//         }
+//     })
+//     if (!skills) {
+//         return 'none found'
+//     }
+//     return skills.dataValues
+// }
+
+// ;(async () => {
+//   // ...
+//     const UserSkills = await findUserSkills(req.body.userSkillsId)
+//     console.log(UserSkills)
+//   // ...
+// })()
