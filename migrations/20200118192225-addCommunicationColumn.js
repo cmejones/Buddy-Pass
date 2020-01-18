b10db3e60ffb19fb5be61638b6335ce88519fade
Leah H <harrichl@gmail.com>
2020-01-18 14:24:43 -0500

    add photo and communication column

---
+++ migrations/20200118192225-addCommunicationColumn.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+        'users',
+        'communication',
+        Sequelize.STRING
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+        'users',
+        'communication'
+    );
+  }
+};
